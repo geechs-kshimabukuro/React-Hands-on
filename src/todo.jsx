0771f7b02e1ffee62cd89c897872886875ea91a0
kabotya <kiyono.shima@gmail.com>
2016-06-22 18:08:28 +0900

    step5
    formで新しいタスクを追加できるようにしましょう

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,8 +1,23 @@
 var TODOForm = React.createClass({
+	getInitialState: function() {
+		return {text: ''};
+	},
+	onChange: function(e) {
+		this.setState({text: e.target.value});
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var newTask = this.state.text;
+		this.props.onAdd(newTask);
+		this.setState({text: ''});
+	},
 	render: function() {
 		return (
 			<div className="todoform">
-				ここがtaskを入力するformになります。
+				<form onSubmit={this.handleSubmit}>
+					<input onChange={this.onChange} value={this.state.text}/>
+					<button>add</button>
+				</form>
 			</div>
 		);
 	}
@@ -27,12 +42,17 @@ var TODOList = React.createClass({
 
 var TODOApp = React.createClass({
 	getInitialState: function() {
-		return { todos: [{id: Date.now(), task: 'hoge', status: 0}], text: ''}
+		return { todos: [{id: Date.now(), task: 'hoge', status: 0}] }
+	},
+	onAdd: function(newTask) {
+		this.setState({
+			todos: this.state.todos.concat({id: Date.now(), task: newTask, status: 0})
+		});
 	},
 	render: function() {
 		return (
 			<div className="todoapp">
-				<TODOForm />
+				<TODOForm onAdd={this.onAdd}/>
 				<TODOList todos={this.state.todos}/>
 			</div>
 		);
